22a4b97fe25cf79add876ea64c45ca1f43c23243
Rehan Mahat <rehanmahat999@gmail.com>
2024-08-01 11:42:01 +0530

    fixes

--- src/connection/db.js
+++ src/connection/db.js
@@ -4,10 +4,7 @@ const mongoose = require('mongoose');
 const connectDb = async()=>{
     try {
        
-        const connect = await mongoose.connect(process.env.CONNECTION,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-          });
+        const connect = await mongoose.connect(process.env.CONNECTION,);
         console.log(" Database connected: " , connect.connection.host, connect.connection.name);
     } catch (error) {
         console.log(error);
